bb9626f2f616f06b13b9f7161b18f0b8e32dea60
pedrolima <pedro.v.lima@inesctec.pt>
2025-01-06 10:54:49 +0000

    feat - include option of seeing only the content within the bbox

--- examples/run_show_bbox.sh
+++ examples/run_show_bbox.sh
@@ -7,4 +7,6 @@ PATIENT_ID="Breast_MRI_052"
 IMAGE_PATH="$MAIN_PATH/nrrd_images/${PATIENT_ID}/post_1.nrrd"
 MASK_PATH="$MAIN_PATH/nrrd_masks/output_${PATIENT_ID}.seg.nrrd"
 
-python3 utils/show_bbox_nrrd.py --image-path "$IMAGE_PATH" --mask-path "$MASK_PATH"
+MODE="cropped" # full, cropped
+
+python3 utils/show_bbox_nrrd.py --mode "$MODE" --image-path "$IMAGE_PATH" --mask-path "$MASK_PATH"
--- utils/show_bbox_nrrd.py
+++ utils/show_bbox_nrrd.py
@@ -1,8 +1,16 @@
 import argparse
+import numpy as np
 import napari
 import SimpleITK as sitk
 
-def visualize_3d_with_napari(image_path, mask_path):
+
+def extract_bounding_box(seg_array):
+    coords = np.argwhere(seg_array > 0)
+    start = coords.min(axis=0)
+    end = coords.max(axis=0) + 1  # include the last index
+    return start, end
+
+def visualize_3d_with_napari(image_path, mask_path, mode):
 
     image = sitk.ReadImage(image_path)
     mask = sitk.ReadImage(mask_path)
@@ -12,16 +20,22 @@ def visualize_3d_with_napari(image_path, mask_path):
 
     assert image_array.shape == mask_array.shape, "Image and mask dimensions must match!"
 
-    with napari.gui_qt():
-        viewer = napari.Viewer(ndisplay=3)  # 3D mode
-        viewer.add_image(image_array, name="MRI Image", colormap="gray", rendering="mip")
-        viewer.add_labels(mask_array, name="Mask", opacity=0.5)
+    if mode == "cropped":
+        start, end = extract_bounding_box(mask_array)
+        image_array = image_array[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
+        mask_array = mask_array[start[0]:end[0], start[1]:end[1], start[2]:end[2]]
+
+    viewer = napari.Viewer(ndisplay=3)  # 3D mode
+    viewer.add_image(image_array, name="MRI Image", colormap="gray", rendering="mip")
+    viewer.add_labels(mask_array, name="Mask", opacity=0.5)
+    napari.run()
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--image-path", required=True)
     parser.add_argument("--mask-path", required=True)
+    parser.add_argument("--mode", required=True, default="full", choices=["full", "cropped"])
     args = parser.parse_args()
 
-    visualize_3d_with_napari(args.image_path, args.mask_path)
+    visualize_3d_with_napari(args.image_path, args.mask_path, args.mode)
