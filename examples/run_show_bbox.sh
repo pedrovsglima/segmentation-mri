d1e382f7aa7e5c4412c315b122a1f6f1de284a57
pedrolima <pedro.v.lima@inesctec.pt>
2025-01-12 18:34:54 +0000

    chore - update to include bbox args

--- examples/run_show_bbox.sh
+++ examples/run_show_bbox.sh
@@ -5,8 +5,9 @@ MAIN_PATH=$(grep '^MAIN_PATH=' .env | cut -d '=' -f2-)
 PATIENT_ID="Breast_MRI_052"
 
 IMAGE_PATH="$MAIN_PATH/nrrd_images/${PATIENT_ID}/post_1.nrrd"
-MASK_PATH="$MAIN_PATH/nrrd_masks/output_${PATIENT_ID}.seg.nrrd"
+BBOX_PATH="$MAIN_PATH/masks_threshold/${PATIENT_ID}.seg.nrrd"
+MASK_PATH="$MAIN_PATH/nrrd_masks/${PATIENT_ID}.seg.nrrd"
 
 MODE="cropped" # full, cropped
 
-python3 utils/show_bbox_nrrd.py --mode "$MODE" --image-path "$IMAGE_PATH" --mask-path "$MASK_PATH"
+python3 utils/show_bbox_nrrd.py --mode "$MODE" --image-path "$IMAGE_PATH" --bbox-path "$BBOX_PATH" --mask-path "$MASK_PATH"
